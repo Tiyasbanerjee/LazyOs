f1e400c3d786eb3b885cfb1b9f2c67000fe78418
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2026-08-20 12:48:12 +0530

    added dragging ability and aldo wrote 2 specific html and css class for close btn and the window header.

--- script.js
+++ script.js
@@ -4,5 +4,48 @@ function updateTime() {
     let currentTime = new Date().toLocaleTimeString();
     timeElement.textContent = currentTime;
 }
-
+// the clock will update every second
 setInterval(updateTime, 1000);
+
+
+function dragElement(elmnt){
+    let initX = 0;
+    let initY = 0;
+    let currentX = 0;
+    let currentY = 0;
+    
+    if (document.getElementById(elmnt.id + "header")) {
+        document.getElementById(elmnt.id + "header").onmousedown = StartDragging;
+    }else{
+        elmnt.onmousedown = StartDragging;
+    }
+
+    function StartDragging(e){
+        e = e || window.event;
+        e.preventDefault();
+
+        initX = e.clientX;
+        initY = e.clientY;
+
+        document.onmouseup = stopDragging;
+        document.onmousemove = ElementDrag;
+    }
+    function ElementDrag(e){
+        e=e|| window.event;
+        e.preventDefault();
+        currentX = initX - e.clientX;
+        currentY = initY - e.clientY;
+        initX = e.clientX;
+        initY = e.clientY;
+
+        elmnt.style.top = (elmnt.offsetTop - currentY) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - currentX) + "px";
+    }
+    function stopDragging(){
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+// its the logic to move elements
+dragElement(document.getElementById("start-window"));
+
